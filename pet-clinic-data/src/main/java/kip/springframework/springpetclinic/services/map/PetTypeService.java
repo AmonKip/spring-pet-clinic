c599bdfe0b95cbf64e70877db14118f8dbad7d6f
KIPROTICH <kiprotich87@yahoo.com>
2018-10-11 22:13:40 -0400

    Added PetType Service and PetType service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/kip/springframework/springpetclinic/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package kip.springframework.springpetclinic.services.map;
+
+import kip.springframework.springpetclinic.model.PetType;
+import kip.springframework.springpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/kip/springframework/springpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package kip.springframework.springpetclinic.services.map;
+
+import kip.springframework.springpetclinic.model.PetType;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends  AbstractMapService<PetType, Long> implements  PetTypeService{
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+       super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+       super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
