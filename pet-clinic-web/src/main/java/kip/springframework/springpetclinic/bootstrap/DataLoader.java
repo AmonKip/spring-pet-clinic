40757a4d0aa2cb72933057951eaf85fc887996bf
KIPROTICH <kiprotich87@yahoo.com>
2018-10-12 21:50:40 -0400

    Added PetType to bootstrap. Closes #34

--- pet-clinic-web/src/main/java/kip/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kip/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,11 @@
 package kip.springframework.springpetclinic.bootstrap;
 
 import kip.springframework.springpetclinic.model.Owner;
+import kip.springframework.springpetclinic.model.PetType;
 import kip.springframework.springpetclinic.model.Vet;
 import kip.springframework.springpetclinic.services.OwnerService;
 import kip.springframework.springpetclinic.services.VetService;
+import kip.springframework.springpetclinic.services.map.PetTypeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setId(1L);
