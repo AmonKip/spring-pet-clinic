ea7b8323c1e9be42921c549fb93a0a946561f434
KIPROTICH <kiprotich87@yahoo.com>
2018-10-13 23:57:10 -0400

    Added Spring Data JPA Service for Vets. Closes #41

---
+++ pet-clinic-data/src/main/java/kip/springframework/springpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package kip.springframework.springpetclinic.services.springdatajpa;
+
+import kip.springframework.springpetclinic.model.Vet;
+import kip.springframework.springpetclinic.repositories.VetRepository;
+import kip.springframework.springpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdataJpa")
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
